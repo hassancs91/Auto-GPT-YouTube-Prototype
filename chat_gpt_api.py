c460b74aaa9c0fe866df143908a9bda4bcbfda03
Hasan Aboul Hasan <hassan.cs91@gmail.com>
2024-01-06 11:07:41 +0200

    update openai function

--- chat_gpt_api.py
+++ chat_gpt_api.py
@@ -5,7 +5,7 @@ openai.api_key = "sk-XXX"
 selected_model = "gpt-3.5-turbo"
 
 def basic_generation(user_prompt):
-    completion = openai.ChatCompletion.create(
+    completion = openai.chat.completions.create(
         model=selected_model,
         messages=[
             {"role": "user", "content": user_prompt}
